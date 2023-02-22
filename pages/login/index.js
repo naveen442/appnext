b8f125b6727b5949f689b4d9105835e8e746c8b4
naveen442 <naveenrp93@gmail.com>
2023-02-22 10:29:07 +0530

    final

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
-import Script from 'next/script'
+import Script from 'next/script';
+
+import Form from "react-bootstrap/Form";
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox,
+    MDBIcon
+  }
+  from 'mdb-react-ui-kit';
+import { Image } from 'react-bootstrap';
 const index = () => {
+  const clearData = {
+   
+    mobilenumber: "",
+};
+let [formData, setFormData] = useState(clearData);
+    const [formErrors, setErrors] = useState({});
+
+
+function formDataAdd(e) {
+  e.preventDefault();
+  setErrors(validate(formData));
+  const formInfo = {
+      fullName: formData.fullName,
+      email: formData.email,
+      designation: formData.designation,
+      mobilenumber: formData. mobilenumber,
+  };
+  console.log(formInfo);
+
+  setFormData(clearData);
+}
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/
+    const mobile = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    if (!values.fullName) {
+        errors.fullName = "Username is required";
+    }
+    if (!values.email) {
+        errors.email = "email is required";
+    }
+    else if (!regex.test(values.email)) {
+        errors.email = "this is not a valid email format";
+    }
+    if (!values.designation) {
+        errors.designation = "designation is required";
+    }
+    if (!values. mobilenumber) {
+        errors. mobilenumber= "mobile number is required";
+    }
+    else if (!mobile.test(values.mobilenumber)) {
+        errors.mobilenumber = "mobile number is incorrect"
+    }
+    return errors;
+}
   return (
     <>
 
@@ -17,13 +77,90 @@ const index = () => {
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></Script>
       <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></Script>
 
-    <div>
-      <div className="page-content-wrapper section-space--inner--120">
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-md-6 mx-auto">
-                    <div className="loginwrap">
-                        <div className="contact-content mb-5 ">
+
+
+      
+  <div className="container-fluid ">
+    <div className="row overall">
+      <div className="col-sm-6 text-black">
+
+      
+
+        <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+          <form className='w-100 my-5'>
+
+            <h3 className="fw-normal mb-3 pb-3 text-center" style={{letterSpacing: "1px"}}>Log in</h3>
+
+            <div className="form-outline mb-4">
+            
+            <Form.Label>Mobile Number</Form.Label>
+                                            <Form.Control
+                                                onChange={(event) => {
+                                                    setFormData({
+                                                        ...formData,
+                                                        mobilenumber: event.target.value,
+                                                    });
+                                                }}
+                                                type="text"
+                                                className="form-style01"
+                                                placeholder="mobile No"
+                                                value={formData. mobilenumber}
+                                            />
+                                              <p className="required">{formErrors. mobilenumber}</p>
+             
+            </div>
+
+           
+
+            <div className="pt-1 mb-4">
+            <button 
+                                type="button"
+                                onClick={formDataAdd}
+                                className="btn btn-info bg-primary font-white d-block w-100"
+                            >
+                                Submit
+                            </button>
+            </div>
+
+            {/* <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Forgot password?</a></p> */}
+  
+            <div class="text-center">
+                  <p>or</p>
+                  <p> Login with:</p>
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>
+
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-google"></i>
+                  </button>
+
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-twitter"></i>
+                  </button>
+
+                 
+                </div>
+          </form>
+
+        </div>
+
+      </div>
+      <div className="col-sm-6 px-0 d-none d-sm-block">
+        <Image src="./assets/login.jpeg" className="img-fluid"/>
+      </div>
+    </div>
+  </div>
+
+
+    {/* <div>
+      <div classNameName="page-content-wrapper section-space--inner--120">
+        <div classNameName="container">
+            <div classNameName="row">
+                <div classNameName="col-12 col-md-6 mx-auto">
+                    <div classNameName="loginwrap">
+                        <div classNameName="contact-content mb-5 ">
                             <h2 className="title text-center">Login</h2>
                             <p className="subtitle text-center">Please enter the your details</p>
                         </div>
@@ -59,9 +196,11 @@ const index = () => {
             </div>
         </div>
     </div>
-    </div>
+    </div> */}
+
     </>
+
   )
-}
+  }
 
 export default index
